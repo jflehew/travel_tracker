3624489a36b2679301eff96e6442f29611932bdd
jflehew <Johnflehew@gmail.com>
2025-05-06 20:47:00 -0600

    cleaned out the comments

--- server/src/main/java/com/nathan/traveltracker/controllers/LoginController.java
+++ server/src/main/java/com/nathan/traveltracker/controllers/LoginController.java
@@ -16,50 +16,30 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-// import org.springframework.stereotype.Controller;
-// import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-// import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
-// @Controller
 @RequestMapping("/api/users")
 public class LoginController {
-    //Home Page Easy Change
-    // String Homepage = "/home";
 
     @Autowired
     private UserService userService;
-//Login Page
-    // @GetMapping("/")
-    // public String index(Model model) {
-    //     model.addAttribute("newUser", new User());
-    //     model.addAttribute("newLogin", new LoginUser());
-    //     return "index.jsp";
-    // }
 
-//Register Account POST method
+
     @PostMapping("/register")
     public ResponseEntity<?> register(@Valid @RequestBody User newUser,
                             BindingResult result,
-                            // Model model,
                             HttpSession session) {
 
         userService.register(newUser, result);
-        // System.out.println("Received user: " + newUser);
 
         if(result.hasErrors()) {
-            // model.addAttribute("newLogin", new LoginUser());
-            // List<String> errorMessages = result.getAllErrors().stream()
-            //     .map(error -> error.getDefaultMessage())
-            //     .toList();
             Map<String, String> userErrors = new HashMap<>();
             result.getFieldErrors().forEach(err -> {
                 userErrors.put(err.getField(), err.getDefaultMessage());
@@ -70,18 +50,13 @@ public class LoginController {
         }
 
         UserDTO responseUser = new UserDTO(newUser);
-        // System.out.println(responseUser);
-
-        //Log in User after making account
         session.setAttribute("user", newUser);
         return ResponseEntity.ok(responseUser);
     }
 
-//Login Method
     @PostMapping("/login")
     public ResponseEntity<?> login(@Valid @RequestBody LoginUser newLogin,
                         BindingResult result,
-                        // Model model,
                         HttpSession session) {
         User user = userService.login(newLogin, result);
 
@@ -94,7 +69,6 @@ public class LoginController {
                 .badRequest()
                 .body(Map.of("errors", loginErrors));
         }
-        //Log in User
         UserDTO responseUser = new UserDTO(user);
         session.setAttribute("user", user);
         return ResponseEntity.ok(responseUser);
